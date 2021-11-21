4f54bad74b47e6d3fcd9c983e49dd520bf7bcd40
bbroger1 <bb.roger@rcssoft.com.br>
2021-11-21 20:16:17 -0300

    Incluída a lógica de registro do usuário

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,32 +4,40 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use App\Resource\Auth\UserResource;
-use App\Resource\Auth\UserResourceCollection;
-use App\Http\Requests\Auth\StoreUpdateUserRequest;
 use App\Models\User;
-use App\Services\ResponseService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    private $user;
-
-    public function __constructor(User $user): void
+    public function store(Request $request)
     {
-        $this->user = $user;
-    }
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required|max:191|string',
+            'email'    => 'required|max:191|email|unique:users,email',
+            'password' => 'required|min:6|string',
+            'passwordConfirm' => 'required|min:6|same:password'
+        ]);
 
-    public function store(StoreUpdateUserRequest $request)
-    {
-        if (!$data = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ])) {
-            return ResponseService::customMessage('auth.store', $id = null, 'Não foi possível criar o usuário');
-        };
-        $data['token'] = $data->createToken($data->email . '_Token')->plainTextToken;
-        return new UserResource($data, array('type' => 'store', 'route' => 'auth.store'));
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        }
+
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken($user->email . '_Token')->plainTextToken;
+
+        return response()->json([
+            'status'    => 200,
+            'username'  => $user->name,
+            'token'     => $token,
+            'message'   => 'Registered Successfully'
+        ]);
     }
 }
