d793dca7bdd9fbd570ccf7b5fa54fa28706d9113
bbroger1 <bb.roger@rcssoft.com.br>
2021-11-21 21:47:11 -0300

    incluída lógica de logout do site

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
